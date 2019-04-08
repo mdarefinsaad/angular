0c38d2e6b9e34a2e992b958315b74eb0e99a0e4e
unknown <mdarefinsaad@gmail.com>
2019-04-08 19:38:06 +0600

    new branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  /*External Css*/
+  /*styleUrls: ['./app.component.css']*/
+  /* Inline css*/
+  styles : [`
+    h3{
+      color :red;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'my-first-app';
+
 }
